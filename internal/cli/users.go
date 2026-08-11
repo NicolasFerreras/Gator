c20605560dbd7819646da8e331c5aa28b344cc4d
NicolasFerreras <nicolas.ferreras@example.com>
2026-08-11 14:03:46 -0300

    feat: implementar handlers addfeed y feeds
    Handlers:
    - handlerAddfeed: agregar feed a DB
    - handlerFeeds: listar feeds registrados
    
    Testing:
    - go build ./... → OK

--- internal/cli/users.go
+++ internal/cli/users.go
@@ -10,12 +10,28 @@ func handlerUsers(state *State, cmd command) error {
 	if err != nil {
 		return ErrDBGetUsers(err)
 	}
+	currentUser, err := GetCurrentUser(state)
+	if err != nil {
+		return fmt.Errorf("failed to get current user: %v", err)
+	}
 	for _, user := range users {
-		if user.Username == state.Config.CurrentUserName {
+		if user.Username == currentUser {
 			fmt.Printf("* %s (current)\n", user.Username)
 		} else {
 			fmt.Printf("* %s\n", user.Username)
 		}
 	}
 	return nil
+}
+
+func GetCurrentUser(state *State) (string, error) {
+	currentUser := state.Config.CurrentUserName
+	if currentUser == "" {
+		return "", fmt.Errorf("no current user set")
+	}
+	currentUserData, err := state.Db.GetUserByUsername(context.Background(), currentUser)
+	if err != nil {
+		return "", fmt.Errorf("failed to get current user from database: %v", err)
+	}
+	return currentUserData.Username, nil
 }
